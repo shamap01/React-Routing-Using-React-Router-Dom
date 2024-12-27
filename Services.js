ac2b830803bb5c7673dc693c49eb45dc9b69ca42
Shama Parveen <139287494+shamap01@users.noreply.github.com>
2024-12-27 06:22:42 +0530

    Create Services.js

---
+++ Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2>Services Page</h2>
+            <p>This is the services page.</p>
+        </div>
+    );
+};
+
+export default Services;
